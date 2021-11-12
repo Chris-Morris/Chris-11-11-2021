6c65fb738ab2406fb069e3a0aba7a4b4565b554c
Chris Morris <chrismorris87@hotmail.co.uk>
2021-11-12 23:39:25 +0000

    Prepare files for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const products = require('./routes/api/products');
 
 const app = express();
@@ -13,6 +13,15 @@ const db = require("./config/config").mongoURI;
 
 // Mount Router
 app.use('/api/products', products);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
